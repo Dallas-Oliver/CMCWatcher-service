c29ee36ee38848bb351447f5c3ca4b0766e03d6b
Dallas Oliver <dallas.oliver@everbridge.com>
2021-08-01 22:43:04 -0400

    fdfdf

--- Server.js
+++ Server.js
@@ -6,22 +6,27 @@ const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
 const cmcHost = "https://pro-api.coinmarketcap.com"
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//get all coins data
-app.get("/latest", async (request, response) => {
-    const res = await fetch(`${cmcHost}/v1/cryptocurrency/listings/latest?cryptocurrency_type=coins`, {
+
+//get 10 coins data sorted by price in ascending order
+app.post("/latest", async (request, response) => {
+    const sortBy = request.body.sortBy;
+    const sortDir = request.body.sortDir;
+    const limit = request.body.limit;
+    const res = await fetch(`${cmcHost}/v1/cryptocurrency/listings/latest?cryptocurrency_type=coins&sort=${sortBy}&sort_dir=${sortDir}&limit=${limit}`, {
         "method": "GET",
         "headers": {
             "X-CMC_PRO_API_KEY": apiKey
         }
-
     });
     const json = await res.json();
 
-
-    response.send(json)
+    response.json(json.data);
 });
 
+//get data for specific coin
 app.get("/latest/:coinSlug", async (request, response) => {
     const slug = request.params.coinSlug;
 
